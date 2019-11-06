fc55b3740fe033e3df9f2ba31c45e4dfff7b184f
Flavian DESVERNE <flavian.desverne@epitech.eu>
2019-11-06 15:52:03 +0100

    add -e flag to generate command

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,19 +1,35 @@
 import { Command, flags } from '@oclif/command'
 import { findServerEntryPoint } from '../utils'
 import { spawnSync } from 'child_process'
+import * as path from 'path'
+import * as fs from 'fs'
 
 export class Generate extends Command {
   static description = 'Generate the artifacts'
 
   static examples = [`$ pumpkins generate`]
 
-  static flags = {}
+  static flags = {
+    entrypoint: flags.string({ char: 'e' }),
+  }
 
   static args = []
 
   async run() {
     const { args, flags } = this.parse(Generate)
-    const entryPoint = findServerEntryPoint()
+    const entryPoint = flags.entrypoint
+      ? path.join(process.cwd(), flags.entrypoint)
+      : findServerEntryPoint()
+
+    if (!fs.existsSync(entryPoint)) {
+      this.error(
+        `🎃  Entry point "${path.relative(
+          process.cwd(),
+          entryPoint,
+        )}" does not exist`,
+        { exit: 1 },
+      )
+    }
 
     spawnSync('ts-node', [entryPoint], {
       env: {
