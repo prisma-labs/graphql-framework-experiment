c93a4dbdb46300fd07e1e01dab32cb48b77599ae
Jason Kuhrt <jasonkuhrt@me.com>
2019-11-06 18:01:50 -0500

    feat: power dev command with ts-node-dev

--- src/cli/dev.ts
+++ src/cli/dev.ts
@@ -1,17 +1,28 @@
-import { Command, flags } from '@oclif/command'
+import { Command } from '@oclif/command'
+import startTSNodeDev from 'ts-node-dev'
+import { findServerEntryPoint, findConfigFile, readTsConfig } from '../utils'
 
 export class Dev extends Command {
   static description = 'describe the command here'
-
   static examples = [`$ pumpkins dev`]
-
   static flags = {}
-
   static args = []
 
   async run() {
-    const { args, flags } = this.parse(Dev)
+    // const { args, flags } = this.parse(Dev)
+    const tsConfig = readTsConfig()
+    const entryPoint = findServerEntryPoint(tsConfig)
 
-    this.log('todo')
+    // Difficultish API to use because no docs or typings
+    // Refer to these source files, top-down by caller order:
+    //
+    // - https://github.com/whitecolor/ts-node-dev/blob/master/bin/ts-node-dev
+    // - https://github.com/whitecolor/ts-node-dev/blob/master/lib/index.js
+    // - https://github.com/whitecolor/ts-node-dev/blob/master/lib/compiler.js
+    //
+    startTSNodeDev(entryPoint, [], [], {
+      'tree-kill': true,
+      'transpile-only': true,
+    })
   }
 }
