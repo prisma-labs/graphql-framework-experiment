91e45c16ba7a2bf3bc3cfcae9cf561d14b0fc257
Jason Kuhrt <jasonkuhrt@me.com>
2019-11-13 09:29:28 -0500

    chore: format

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -5,16 +5,13 @@ type DebugType = 'app' | 'schema' | 'prisma'
 const DebugType: Record<DebugType, string> = {
   app: 'pumpkins:app',
   schema: 'pumpkins:schema',
-  prisma: "pumpkins:prisma"
+  prisma: 'pumpkins:prisma',
 }
 
 export const debug: Record<DebugType, Debug.Debugger> = Object.keys(
   DebugType
-).reduce(
-  (acc, key) => {
-    acc[key] = Debug(DebugType[key as DebugType])
+).reduce((acc, key) => {
+  acc[key] = Debug(DebugType[key as DebugType])
 
-    return acc
-  },
-  {} as any
-)
+  return acc
+}, {} as any)
