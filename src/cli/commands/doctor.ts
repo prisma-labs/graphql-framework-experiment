4a0db0544f6eebb078dae746fd2ca9590abb61af
Jason Kuhrt <jasonkuhrt@me.com>
2019-11-29 14:05:20 -0500

    refator: doctor

--- src/cli/commands/doctor.ts
+++ src/cli/commands/doctor.ts
@@ -10,31 +10,22 @@ export class Doctor implements Command {
   }
 
   async parse() {
-    const frameworkDotFolder = '.pumpkins'
-    const git = Git(process.cwd())
+    await tsconfig()
+  }
+}
 
-    console.log(chalk.bold('-- .gitignore --'))
-    if (await git.checkIsRepo()) {
-      if ((await git.checkIgnore([frameworkDotFolder])).length === 0) {
-        console.log(
-          chalk`{yellow Warning:}  Please add ${frameworkDotFolder} to your gitignore file`
-        )
-      } else {
-        console.log(
-          chalk`{green OK:}  ${frameworkDotFolder} is git-ignored correctly`
-        )
-      }
-    }
-
-    console.log(chalk.bold('-- tsconfig.json --'))
-    const layout = await scan()
-    const result = await findOrScaffoldTsConfig(layout, {
-      exitAfterError: false,
-    })
-    if (result === 'success') {
-      console.log(
-        chalk`{green OK:} "tsconfig.json" is present and in the right directory`
-      )
-    }
+/**
+ * Check that a tsconfig file is present and scaffold one if it is not.
+ */
+async function tsconfig() {
+  console.log(chalk.bold('-- tsconfig.json --'))
+  const layout = await scan()
+  const result = await findOrScaffoldTsConfig(layout, {
+    exitAfterError: false,
+  })
+  if (result === 'success') {
+    console.log(
+      chalk`{green OK:} "tsconfig.json" is present and in the right directory`
+    )
   }
 }
