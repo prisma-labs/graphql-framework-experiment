735849ed402e451d892276431d79b0fe4b1ba098
Jason Kuhrt <jasonkuhrt@me.com>
2019-12-12 00:11:03 -0500

    fix

--- src/cli/commands/create.tsx
+++ src/cli/commands/create.tsx
@@ -135,7 +135,7 @@ export async function runBootstrapper(
 
   // TODO spawn create hand off
   console.log('select a template to continue with...')
-  const templatingChild = spawn('yarn', ['-s', 'pumpkins create'], {
+  const templatingChild = spawn('yarn', ['-s', 'pumpkins', 'create'], {
     stdio: 'inherit',
     env: { ...process.env, PUMPKINS_CREATE_HANDOFF: 'true' },
   })
