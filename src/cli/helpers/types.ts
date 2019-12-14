d006da31eea75b787526e57a5ae9b2aeef0f40d0
Jason Kuhrt <jasonkuhrt@me.com>
2019-12-14 12:29:47 -0500

    jsdoc

--- src/cli/helpers/types.ts
+++ src/cli/helpers/types.ts
@@ -6,9 +6,49 @@ export interface Command {
 }
 
 /**
- * Commands
+ * Commands. An object where keys represent command names. Values are command
+ * implementations. The structure is recursive, allowing for nested
+ * sub-commands. String values are allowed as references to other commands at
+ * the same level.
+ *
+ * @example
+ *  {
+ *    a: new ACommand(),
+ *    b: 'a'
+ *  }
+ *
+ *  $ some-cli a
+ *  $ some-cli b # invokes 'a'
+ *
+ * @example
+ *
+ *  {
+ *    a: new ACommand(),
+ *    b: {
+ *      __default: 'b2',
+ *      b1: new B1Command(),
+ *      b2: new B2Command(),
+ *      b3: {
+ *        b31: new FooB31Command(),
+ *        b32: new FooB32Command(),
+ *      }
+ *    }
+ *  }
+ *
+ *  $ some-cli a
+ *  $ some-cli b            # runs `b2` because __default set so
+ *  $ some-cli b b1
+ *  $ some-cli b b2
+ *  $ some-cli b b3         # error, no default set
+ *  $ some-cli b b3 b31
+ *  $ some-cli b b3 b32
+ *
  */
-export type Commands = { [command: string]: Command }
+export type Commands = {
+  // TODO how?
+  // __default?: Command | string
+  [name: string]: Command | Commands | string
+}
 
 export type Dictionary<T> = {
   [key: string]: T
