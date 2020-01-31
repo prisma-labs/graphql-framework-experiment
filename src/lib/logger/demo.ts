3e5869d791c6052e8ef3c3de1de0524a12bcc6dc
Jason Kuhrt <jasonkuhrt@me.com>
2020-01-31 16:23:05 -0500

    feat(logger): format context
    closes #349

--- src/lib/logger/demo.ts
+++ src/lib/logger/demo.ts
@@ -9,9 +9,50 @@ export function demo() {
   const origLevel = logger.settings.level
   logger.settings({ level: 'trace' })
   logger.fatal('foo', { lib: /see/ })
-  logger.error('foo', { har: { mar: 'tek' } })
+  logger.error('foo', {
+    har: { mar: 'tek' },
+    jar: [1, 2, 3, 4, 4, 5, 6, 6, 7, 9, 1, 2, 4, 5, 6, 7, 3, 6, 5, 4],
+    kio: Object.create(null, { [Symbol.toStringTag]: { value: 'foo' } }),
+  })
   logger.warn('foo', { bleep: [1, '2', true] })
   logger.info('foo')
+  logger.debug('foo', {
+    results: [
+      {
+        userId: 1,
+        id: 1,
+        title: 'delectus aut autem',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: 'quis ut nam facilis et officia qui',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: 'fugiat veniam minus',
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: 'et porro tempora',
+        completed: true,
+      },
+      {
+        userId: 1,
+        id: 5,
+        title:
+          'laboriosam mollitia et enim quasi adipisci quia provident illum',
+        completed: false,
+      },
+    ],
+    tri: 'wiz',
+    on: false,
+  })
   logger.debug('foo', { foo: 'bar' })
   logger.trace('foo', { a: 1, b: 2, c: 'three' })
   logger.settings({ level: origLevel })
