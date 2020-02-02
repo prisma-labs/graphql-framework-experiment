88cfa222e17288fb568c89c53f1ff5fb79600595
Jason Kuhrt <jasonkuhrt@me.com>
2020-02-02 08:08:10 -0500

    reduce scope

--- src/lib/logger/prettifier.ts
+++ src/lib/logger/prettifier.ts
@@ -1,5 +1,4 @@
 import chalk, { Chalk } from 'chalk'
-import stripAnsi from 'strip-ansi'
 import * as util from 'util'
 import * as utils from '../utils'
 import * as Level from './level'
@@ -60,45 +59,27 @@ const pathSep = {
 const eventSep = ':'
 
 const contextSep = {
-  singleLine: {
-    symbol: ' --  ',
-    // context = ` ${chalk.gray('⸬')}  ` + context
-    // context = ` ${chalk.gray('•')}  ` + context
-    // context = ` ${chalk.gray('⑊')}  ` + context
-    // context = ` ${chalk.gray('//')}  ` + context
-    // context = ` ${chalk.gray('—')}  ` + context
-    // context = ` ${chalk.gray('~')}  ` + context
-    // context = ` ${chalk.gray('⌀')}  ` + context
-    // context = ` ${chalk.gray('——')}  ` + context
-    // context = ` ${chalk.gray('❯')}  ` + context
-    // context = ` ${chalk.gray('->')}  ` + context
-    // context = ` ${chalk.gray('⌁')}  ` + context
-    // context = ` ${chalk.gray('⋯')}  ` + context
-    // context = ` ${chalk.gray('⌁')}  ` + context
-    // context = ` ${chalk.gray('⟛')}  ` + context
-    color: chalk.gray,
-  },
-  multiline: {
-    symbol: '',
-  },
+  symbol: '--',
+  // context = ` ${chalk.gray('⸬')}  ` + context
+  // context = ` ${chalk.gray('•')}  ` + context
+  // context = ` ${chalk.gray('⑊')}  ` + context
+  // context = ` ${chalk.gray('//')}  ` + context
+  // context = ` ${chalk.gray('—')}  ` + context
+  // context = ` ${chalk.gray('~')}  ` + context
+  // context = ` ${chalk.gray('⌀')}  ` + context
+  // context = ` ${chalk.gray('——')}  ` + context
+  // context = ` ${chalk.gray('❯')}  ` + context
+  // context = ` ${chalk.gray('->')}  ` + context
+  // context = ` ${chalk.gray('⌁')}  ` + context
+  // context = ` ${chalk.gray('⋯')}  ` + context
+  // context = ` ${chalk.gray('⌁')}  ` + context
+  // context = ` ${chalk.gray('⟛')}  ` + context
+  color: chalk.gray,
 }
 
 const contextKeyValSep = {
-  singleLine: {
-    symbol: ': ',
-    color: chalk.gray,
-  },
-  multiline: {
-    symbol: '  ',
-  },
-}
-
-const contextEntrySep = {
-  singleLine: '  ',
-  multiline: {
-    symbol: `\n  | `,
-    color: chalk.gray,
-  },
+  symbol: ': ',
+  color: chalk.gray,
 }
 
 type Options = {
@@ -111,89 +92,35 @@ export function create(options: Options) {
 
 export function render(options: Options, rec: Logger.LogRecord): string {
   const levelLabel = Level.LEVELS_BY_NUM[rec.level].label
-  const style = LEVEL_STYLES[levelLabel]
-
-  // render pre-context
-
   const levelLabelRendered = options.levelLabel
     ? ' ' + utils.spanSpace(5, levelLabel) + ' '
     : ' '
   const path = rec.path.join(renderEl(pathSep))
 
-  const renderedPreContext = `${style.color(
-    `${style.badge}${levelLabelRendered}${path}`
-  )}${chalk.gray(eventSep)}${rec.event}`
-
   // render context
 
-  const availableContextColumns =
-    process.stdout.columns - stripAnsi(renderedPreContext).length
-  let contextColumnsConsumed = 0
+  let contextRendered = Object.entries(rec.context)
+    .map(
+      e =>
+        `${chalk.gray(e[0])}${renderEl(contextKeyValSep)}${util.inspect(e[1], {
+          colors: true,
+          getters: true,
+          depth: 20,
+        })}`
+    )
+    .join('  ')
 
-  const contextEntries = Object.entries(rec.context)
-  let widestKey = 0
-
-  const contextEntriesRendered = contextEntries.map(([key, value]) => {
-    contextColumnsConsumed +=
-      key.length + contextKeyValSep.singleLine.symbol.length
-    if (key.length > widestKey) widestKey = key.length
-
-    const valueRendered = `${util.inspect(value, {
-      breakLength: availableContextColumns,
-      colors: true,
-      getters: true,
-      depth: 20,
-    })}`
-
-    contextColumnsConsumed += stripAnsi(valueRendered).length
-
-    return [key, valueRendered]
-  })
-
-  const contextFitsSingleLine =
-    contextColumnsConsumed <= availableContextColumns
-
-  let contextRendered = ''
-  if (contextEntries.length > 0) {
-    if (contextFitsSingleLine) {
-      contextRendered =
-        renderEl(contextSep.singleLine) +
-        contextEntriesRendered
-          .map(
-            ([key, value]) =>
-              `${chalk.gray(key)}${renderEl(
-                contextKeyValSep.singleLine
-              )}${value}`
-          )
-          .join(contextEntrySep.singleLine)
-    } else {
-      contextRendered =
-        renderEl(contextSep.multiline) +
-        renderEl(contextEntrySep.multiline) +
-        contextEntriesRendered
-          .map(
-            ([key, value]) =>
-              `${chalk.gray(utils.spanSpace(widestKey, key))}${renderEl(
-                contextKeyValSep.multiline
-              )}${formatBlock(
-                {
-                  leftSpineSymbol: { color: chalk.gray, symbol: '  | ' }, // todo unify with el def above,
-                  excludeFirstLine: true,
-                  indent:
-                    widestKey +
-                    contextKeyValSep.multiline.symbol.length +
-                    contextEntrySep.multiline.symbol.length,
-                },
-                value
-              )}`
-          )
-          .join(renderEl(contextEntrySep.multiline))
-    }
+  if (contextRendered) {
+    contextRendered = ` ${renderEl(contextSep)}  ` + contextRendered
   }
+
+  const style = LEVEL_STYLES[levelLabel]
 
   // put it together
 
-  return `${renderedPreContext} ${contextRendered}\n`
+  return `${style.color(
+    `${style.badge}${levelLabelRendered}${path}`
+  )}${chalk.gray(eventSep)}${rec.event} ${contextRendered}\n`
 }
 
 type El = {
@@ -203,76 +130,4 @@ type El = {
 
 function renderEl(el: El) {
   return el.color ? el.color(el.symbol) : el.symbol
-}
-
-function formatBlock(
-  opts: {
-    indent?: number
-    excludeFirstLine?: boolean
-    leftSpineSymbol?: El
-  },
-  block: string
-): string {
-  const [first, ...rest] = block.split('\n')
-  if (rest.length === 0) return first
-  const linesToProcess =
-    opts.excludeFirstLine === true ? rest : (rest.unshift(first), rest)
-  const prefix = opts.leftSpineSymbol?.symbol ?? ''
-  const indent =
-    opts.indent !== undefined
-      ? range(opts.indent - prefix.length)
-          .map(constant(' '))
-          .join('')
-      : ''
-  const linesProcessed = opts.excludeFirstLine === true ? [first] : []
-  for (const line of linesToProcess) {
-    const prefixRendered = opts.leftSpineSymbol?.color?.(prefix) ?? prefix
-    linesProcessed.push(prefixRendered + indent + line)
-  }
-  return linesProcessed.join('\n')
-}
-
-// /**
-//  * Indent a string that spans multiple lines except for the first line.
-//  */
-// function indentBlockTail(size: number, block: string): string {
-//   // TODO optimize!!!!!
-//   const [first, ...rest] = block.split('\n')
-//   if (rest.length === 0) return first
-//   return first + '\n' + indentBlock(size, rest.join('\n'))
-// }
-
-// /**
-//  * Indent a string that spans multiple lines.
-//  */
-// function indentBlock(size: number, block: string): string {
-//   return block
-//     .split('\n')
-//     .map(
-//       line =>
-//         range(size)
-//           .map(constant(' '))
-//           .join('') + line
-//     )
-//     .join('\n')
-// }
-
-/**
- * Create a function that will only ever return the given value when called.
- */
-const constant = <T>(x: T): (() => T) => {
-  return function() {
-    return x
-  }
-}
-
-/**
- * Create a range of integers.
- */
-const range = (times: number): number[] => {
-  const list: number[] = []
-  while (list.length < times) {
-    list.push(list.length + 1)
-  }
-  return list
 }
