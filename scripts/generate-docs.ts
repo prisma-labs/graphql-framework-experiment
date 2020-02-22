fa6050291ac2903a5db8d1fcac519c95e805c3bb
Jason Kuhrt <jasonkuhrt@me.com>
2020-02-21 22:17:36 -0500

    add toc gen

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,10 +1,25 @@
 import * as cp from 'child_process'
 import * as fs from 'fs-jetpack'
 
-// prettier-ignore
-const json = cp.spawnSync('tydoc',['project','index', '--json'], { encoding: 'utf8' }).output[1]
-fs.write('api.json', json)
+let result: cp.SpawnSyncReturns<string>
+
+console.log('generating json...')
 
 // prettier-ignore
-const md = cp.spawnSync('tydoc',['project','index'], { encoding: 'utf8' }).output[1]
-fs.write('api.md', md)
+result = cp.spawnSync('tydoc',['project','index', 'testing', 'plugin', '--json'], { encoding: 'utf8' })
+console.log(result.error)
+fs.write('api.json', result.output[1])
+
+console.log('generating markdown...')
+
+// prettier-ignore
+result =  cp.spawnSync('tydoc',['project','index', 'testing', 'plugin'], { encoding: 'utf8' })
+console.log(result.error)
+fs.write('api.md', result.output[1])
+
+console.log('generating markdown table of contents...')
+
+// prettier-ignore
+result = cp.spawnSync('yarn', ['-s', 'doctoc', 'api.md','--title', '# Nexus API Reference'], { encoding: 'utf8' })
+console.log(result.error)
+console.log(result.output[1])
