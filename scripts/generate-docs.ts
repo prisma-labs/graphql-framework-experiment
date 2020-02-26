37ff2d040276df9c08a655e10a8c3e9505e64fb0
Jason Kuhrt <jasonkuhrt@me.com>
2020-02-26 16:45:53 -0500

    fix gen script

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -7,19 +7,22 @@ console.log('generating json...')
 
 // prettier-ignore
 result = cp.spawnSync('tydoc',['project','index', 'testing', 'plugin', '--json'], { encoding: 'utf8' })
-console.log(result.error)
-fs.write('api.json', result.output[1])
+if (result.error) console.error(result.error)
+else if (result.stderr) console.error(result.stderr)
+else fs.write('api.json', result.stdout)
 
 console.log('generating markdown...')
 
 // prettier-ignore
 result =  cp.spawnSync('tydoc',['project','index', 'testing', 'plugin'], { encoding: 'utf8' })
-console.log(result.error)
-fs.write('api.md', result.output[1])
+if (result.error) console.error(result.error)
+else if (result.stderr) console.error(result.stderr)
+else fs.write('api.md', result.stdout)
 
 console.log('generating markdown table of contents...')
 
 // prettier-ignore
 result = cp.spawnSync('yarn', ['-s', 'doctoc', 'api.md','--title', '# Nexus API Reference'], { encoding: 'utf8' })
-console.log(result.error)
-console.log(result.output[1])
+if (result.error) console.error(result.error)
+else if (result.stderr) console.error(result.stderr)
+else console.log(result.stdout)
