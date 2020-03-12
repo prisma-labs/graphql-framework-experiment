c0f353e786ce3a889871133d9aab66a2abd3b024
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-03-12 18:37:10 +0100

    add credits to levenstein implementation

--- src/utils/levenstein.ts
+++ src/utils/levenstein.ts
@@ -1,3 +1,5 @@
+// Borrowed from @nexus/schema
+
 /**
  * Given an invalid input string and a list of valid options, returns a filtered
  * list of valid options sorted based on their similarity with the input.
