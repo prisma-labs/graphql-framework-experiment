2f01dc9ae76a6ec4ccd2e7e135ed4d19b34e926e
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-03-12 17:30:47 +0100

    fix: warn instead of throw if wrong backing type

--- src/lib/backing-types/remapNexusTypes.ts
+++ src/lib/backing-types/remapNexusTypes.ts
@@ -1,22 +1,34 @@
 import * as NexusSchema from '@nexus/schema'
-import { AllNexusTypeDefs } from '../../framework/schema'
+import * as Schema from '../../framework/schema'
+import { rootLogger } from '../../utils/logger'
 import { BackingTypes } from './extract'
 
+const log = rootLogger.child('backing-types')
+
 export function remapNexusTypesWithBackingTypes(
-  types: AllNexusTypeDefs[],
+  types: Schema.AllNexusTypeDefs[],
   backingTypes: BackingTypes
 ) {
   return types.map(t => {
     if (
       NexusSchema.core.isNexusObjectTypeDef(t) ||
       NexusSchema.core.isNexusInterfaceTypeDef(t) ||
-      NexusSchema.core.isNexusUnionTypeDef(t)
+      NexusSchema.core.isNexusUnionTypeDef(t) ||
+      NexusSchema.core.isNexusScalarTypeDef(t) ||
+      NexusSchema.core.isNexusEnumTypeDef(t)
     ) {
       if (typeof t.value.rootTyping === 'string') {
         const filePath = backingTypes[t.value.rootTyping]
 
+        if (t.value.rootTyping.length === 0) {
+          return t
+        }
+
         if (!filePath) {
-          throw new Error(`Could not find rootTyping: ${t.value.rootTyping}`)
+          log.warn(
+            `We could not find the backing type '${t.value.rootTyping}' used in '${t.name}'`
+          )
+          return t
         }
 
         t.value.rootTyping = {
