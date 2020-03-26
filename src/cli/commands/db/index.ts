21c292c24e6bbc6d6c0d70c54177656d89dfabe0
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-03-26 17:51:54 +0100

    feat: remove `nexus db` cli commands.BREAKING CHANGE:From now on, prisma2 cli will need to be used directly by users

--- src/cli/commands/db/index.ts
+++
@@ -1,6 +0,0 @@
-import * as Migrate from './migrate'
-
-export { Migrate }
-export { DbInit as Init } from './init'
-export { DbUi as Ui } from './ui'
-export { DbDefault as Default } from './__default'
