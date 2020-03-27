3da4f8365fa50ade32e6d36394a55152cb268b44
Jason Kuhrt <jasonkuhrt@me.com>
2020-03-27 19:15:02 -0400

    feat: cwd-based project root
    Simplify system and move toward a cli flag for custom.

--- src/lib/project-root.ts
+++ src/lib/project-root.ts
@@ -1,18 +1,6 @@
-import * as Path from 'path'
-
 /**
  * Find project root based on the location of this file
  */
-export function getProjectRoot() {
-  if (process.env.LINK) {
-    return process.cwd()
-  }
-
-  const localPackageJsonDir = Path.dirname(
-    require.resolve('../../package.json')
-  )
-
-  const projectRoot = Path.resolve(localPackageJsonDir, '..', '..')
-
-  return projectRoot
+export function getProjectRoot(): string {
+  return process.cwd()
 }
