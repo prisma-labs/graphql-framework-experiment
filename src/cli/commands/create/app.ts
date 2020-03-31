7884f35567d695f6ca49e474cc604e9486d2baec
Jason Kuhrt <jasonkuhrt@me.com>
2020-03-31 09:50:34 -0400

    fixes

--- src/cli/commands/create/app.ts
+++ src/cli/commands/create/app.ts
@@ -34,7 +34,6 @@ interface ConfigInput {
 
 interface InternalConfig {
   projectName: string
-  nexusVersionExpression: string
 }
 
 /**
@@ -83,18 +82,16 @@ export async function runBootstrapper(
     : undefined
   const packageManagerTypeEnvVar = process.env
     .CREATE_APP_CHOICE_PACKAGE_MANAGER_TYPE as any
-  const nexusVersionExpressionEnvVar =
-    process.env.CREATE_APP_CHOICE_NEXUS_VERSION_EXPRESSION
+  const nexusVersionEnvVar = process.env.CREATE_APP_CHOICE_NEXUS_VERSION
   log.trace('create app user choices pre-filled by env vars?', {
     packageManagerTypeEnvVar,
     databaseTypeEnvVar,
-    nexusVersionExpressionEnvVar: nexusVersionExpressionEnvVar,
+    nexusVersionExpressionEnvVar: nexusVersionEnvVar,
   })
 
   const projectName = configInput?.projectName ?? CWDProjectNameOrGenerate()
 
-  const nexusVersionExpression =
-    nexusVersionExpressionEnvVar ?? `^${ownPackage.version}`
+  const nexusVersion = nexusVersionEnvVar ?? `${ownPackage.version}`
 
   const packageManagerType =
     packageManagerTypeEnvVar ?? (await askForPackageManager())
@@ -122,7 +119,6 @@ export async function runBootstrapper(
 
   const options: InternalConfig = {
     projectName: projectName,
-    nexusVersionExpression: nexusVersionExpression,
     ...configInput,
   }
 
@@ -133,10 +129,12 @@ export async function runBootstrapper(
   log.info('Scaffolding base project files...')
   await scaffoldBaseFiles(layout, options)
 
-  log.info(
-    `Installing nexus@${options.nexusVersionExpression}... (this will take around ~15 seconds)`
-  )
-  await layout.packageManager.installDeps({ require: true })
+  log.info(`Installing nexus (this will take around ~15 seconds)`, {
+    version: nexusVersion,
+  })
+  await layout.packageManager.addDeps([`nexus@${nexusVersion}`], {
+    require: true,
+  })
 
   //
   // install additional deps
@@ -446,9 +444,7 @@ async function scaffoldBaseFiles(
     fs.writeAsync('package.json', {
       name: options.projectName,
       license: 'UNLICENSED',
-      dependencies: {
-        nexus: options.nexusVersionExpression,
-      },
+      dependencies: {},
       scripts: {
         format: "npx prettier --write './**/*.{ts,md}' '!./prisma/**/*.md'",
         dev: 'nexus dev',
