c7f73b236800040a600a030e9363845d81073a8a
Jason Kuhrt <jasonkuhrt@me.com>
2020-04-07 23:02:02 -0400

    do not run craete prisma app yet

--- test/e2e/e2e-create-nexus-app.test.ts
+++ test/e2e/e2e-create-nexus-app.test.ts
@@ -1,8 +1,6 @@
-import { createE2EContext } from '../../src/lib/e2e-testing'
-import { e2ePrismaApp } from '../__helpers/e2e'
+// const ctx = createE2EContext({ localNexus: null })
 
-const ctx = createE2EContext({ localNexus: null })
-
-test('e2e', async () => {
-  await e2ePrismaApp(ctx)
-})
+test.todo('e2e create prisma app')
+// , async () => {
+//   await e2ePrismaApp(ctx)
+// })
--- test/system/e2e-create-nexus-app.test.ts
+++ test/system/e2e-create-nexus-app.test.ts
@@ -1,16 +1,14 @@
-import * as path from 'path'
-import { createE2EContext } from '../../src/lib/e2e-testing'
-import { e2ePrismaApp } from '../__helpers/e2e'
+// const ctx = createE2EContext({
+//   localNexus: {
+//     path: path.join(__dirname, '..', '..'),
+//     createAppWithThis: true,
+//     createPluginWithThis: true,
+//     pluginLinksToThis: true,
+//   },
+// })
 
-const ctx = createE2EContext({
-  localNexus: {
-    path: path.join(__dirname, '..', '..'),
-    createAppWithThis: true,
-    createPluginWithThis: true,
-    pluginLinksToThis: true,
-  },
-})
+// test('e2e', async () => {
+//   await e2ePrismaApp(ctx)
+// })
 
-test('e2e', async () => {
-  await e2ePrismaApp(ctx)
-})
+test.todo('e2e create prisma app')
