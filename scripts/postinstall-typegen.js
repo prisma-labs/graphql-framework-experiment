9b4ce60bc2596eb2d6c40f42d65b10d102622c0b
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-04-27 18:10:14 +0200

    use fork instead of spawn

--- scripts/postinstall-typegen.js
+++ scripts/postinstall-typegen.js
@@ -1,9 +1,9 @@
 const { join } = require('path')
-const { spawn } = require('child_process')
+const { fork } = require('child_process')
 
 const scriptPath = join(__dirname, 'postinstall-typegen-script.js')
 // Run the typegen generation in a sub-process to avoid any potential errors during install
-const cp = spawn('node', [scriptPath], { stdio: 'inherit' })
+const cp = fork(scriptPath, [], { stdio: 'inherit' })
 // Unref the child process to allow NodeJS to gracefully exits immediately.
 // The sub-process has a timeout of 10s before it exits anyway if anything does wrong
 cp.unref()
