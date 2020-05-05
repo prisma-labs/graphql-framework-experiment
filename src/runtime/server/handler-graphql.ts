ecb578eb1a807c5dfc1c8ba21715e649da182b0c
Jason Kuhrt <jasonkuhrt@me.com>
2020-05-05 16:16:30 -0400

    parse graphql params

--- src/runtime/server/handler-graphql.ts
+++ src/runtime/server/handler-graphql.ts
@@ -1,6 +1,8 @@
-import { isLeft } from 'fp-ts/lib/Either'
+import { Either, isLeft, left, right } from 'fp-ts/lib/Either'
 import { execute, parse, Source, validate } from 'graphql'
-import createError from 'http-errors'
+import { IncomingMessage } from 'http'
+import createError, { HttpError } from 'http-errors'
+import url from 'url'
 import { AppState } from '../app'
 import { parseBody } from './parse-body'
 import { NexusRequestHandler } from './server'
@@ -8,20 +10,27 @@ import { sendError, sendSuccess } from './utils'
 
 type CreateHandler = (appState: AppState) => NexusRequestHandler
 
-export const createRequestHandlerGraphQL: CreateHandler = (appState: AppState) => async (req, res) => {
-  const errBody = await parseBody(req)
+type GraphQLParams = {
+  query: null | string
+  variables: null | Record<string, unknown>
+  operationName: null | string
+  raw: boolean
+}
 
-  if (isLeft(errBody)) {
-    return sendError(res, errBody.left)
+export const createRequestHandlerGraphQL: CreateHandler = (appState: AppState) => async (req, res) => {
+  const errParams = await getGraphQLParams(req)
+
+  if (isLeft(errParams)) {
+    return sendError(res, errParams.left)
   }
 
-  const body = errBody.right
+  const params = errParams.right
 
-  if (typeof body.query !== 'string') {
+  if (typeof params.query !== 'string') {
     return sendError(res, createError(400, 'request.body json expected to have a query field'))
   }
 
-  const source = new Source(body.query)
+  const source = new Source(params.query)
 
   let documentAST
   try {
@@ -47,6 +56,7 @@ export const createRequestHandlerGraphQL: CreateHandler = (appState: AppState) =
       schema: appState.assembled!.schema,
       document: documentAST,
       contextValue: context,
+      variableValues: params.variables,
       // todo other options
     })
   } catch (error) {
@@ -54,4 +64,60 @@ export const createRequestHandlerGraphQL: CreateHandler = (appState: AppState) =
   }
 
   sendSuccess(res, result)
+}
+
+/**
+ * Provided a "Request" provided by express or connect (typically a node style
+ * HTTPClientRequest), Promise the GraphQL request parameters.
+ */
+async function getGraphQLParams(request: IncomingMessage): Promise<Either<HttpError, GraphQLParams>> {
+  const bodyData = await parseBody(request)
+  if (isLeft(bodyData)) return bodyData
+  const urlData = (request.url && url.parse(request.url, true).query) || {}
+  return parseGraphQLParams(urlData, bodyData.right)
+}
+
+/**
+ * Helper function to get the GraphQL params from the request.
+ */
+function parseGraphQLParams(
+  urlData: Record<string, unknown>,
+  bodyData: Record<string, unknown>
+): Either<HttpError, GraphQLParams> {
+  let query: string | null
+  const incomingQuery = urlData.query || bodyData.query
+
+  if (typeof incomingQuery === 'string') {
+    query = incomingQuery
+  } else {
+    query = null
+  }
+
+  let variables: null | Record<string, unknown>
+  const incomingVariables = urlData.variables || bodyData.variables
+
+  if (typeof incomingVariables === 'string') {
+    try {
+      variables = JSON.parse(incomingVariables)
+    } catch (error) {
+      return left(createError(400, 'Variables are invalid JSON.'))
+    }
+  } else if (typeof incomingVariables === 'object' && incomingVariables !== null) {
+    variables = incomingVariables as Record<string, unknown>
+  } else {
+    variables = null
+  }
+
+  let operationName
+  const incomingOperationName = urlData.operationName || bodyData.operationName
+
+  if (typeof incomingOperationName === 'string') {
+    operationName = incomingOperationName
+  } else {
+    operationName = null
+  }
+
+  const raw = urlData.raw !== undefined || bodyData.raw !== undefined
+
+  return right({ query, variables, operationName, raw })
 }
