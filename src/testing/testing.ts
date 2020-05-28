65f7a88232386357eb429198cbb9db5febc847fa
Jason Kuhrt <jasonkuhrt@me.com>
2020-05-27 21:40:54 -0400

    refactor: use graphql-request dep

--- src/testing/testing.ts
+++ src/testing/testing.ts
@@ -1,7 +1,7 @@
 import { isLeft } from 'fp-ts/lib/Either'
 import getPort from 'get-port'
+import { GraphQLClient } from 'graphql-request'
 import * as Lo from 'lodash'
-import { GraphQLClient } from '../lib/graphql-client'
 import * as Layout from '../lib/layout'
 import { rootLogger } from '../lib/nexus-logger'
 import * as PluginRuntime from '../lib/plugin'
