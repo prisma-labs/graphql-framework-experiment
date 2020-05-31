b4953e10326991f63d06443d447627472847cb0d
Jason Kuhrt <jasonkuhrt@me.com>
2020-05-31 00:00:50 -0400

    refactor: remove fatals within layout

--- src/lib/layout/index.spec.ts
+++ src/lib/layout/index.spec.ts
@@ -20,7 +20,7 @@ import * as Layout from '.'
 import { FSSpec, writeFSSpec } from '../../lib/testing-utils'
 import { rightOrThrow } from '../glocal/utils'
 import * as TC from '../test-context'
-import { repalceInObject } from '../utils'
+import { repalceInObject, replaceEvery } from '../utils'
 import { NEXUS_TS_LSP_IMPORT_ID } from './tsconfig'
 
 /**
@@ -65,7 +65,12 @@ const ctx = TC.create(
       setup(spec: FSSpec = {}) {
         writeFSSpec(ctx.tmpDir, spec)
       },
-      async scan(opts?: { entrypointPath?: string; buildOutput?: string }) {
+      stripTmpDir(x: object | string) {
+        return typeof x === 'string'
+          ? replaceEvery(x, ctx.tmpDir, '__DYNAMIC__')
+          : repalceInObject(ctx.tmpDir, '__DYNAMIC__', x)
+      },
+      async scanThrow(opts?: { entrypointPath?: string; buildOutput?: string }) {
         const data = rightOrThrow(
           await Layout.create({
             cwd: ctx.tmpDir,
@@ -76,6 +81,14 @@ const ctx = TC.create(
         )
         mockedStdoutBuffer = mockedStdoutBuffer.split(ctx.tmpDir).join('__DYNAMIC__')
         return repalceInObject(ctx.tmpDir, '__DYNAMIC__', data.data)
+      },
+      async scan(opts?: { entrypointPath?: string; buildOutput?: string }) {
+        return Layout.create({
+          cwd: ctx.tmpDir,
+          entrypointPath: opts?.entrypointPath,
+          buildOutputDir: opts?.buildOutput,
+          asBundle: false,
+        }).then((v) => repalceInObject(ctx.tmpDir, '__DYNAMIC__', v))
       },
     }
   })
@@ -89,7 +102,7 @@ it('fails if empty file tree', async () => {
   ctx.setup()
 
   try {
-    await ctx.scan()
+    await ctx.scanThrow()
   } catch (err) {
     expect(err.message).toContain("Path you want to find stuff in doesn't exist")
   }
@@ -101,7 +114,7 @@ describe('tsconfig', () => {
   })
 
   it('will scaffold tsconfig if not present', async () => {
-    await ctx.scan()
+    await ctx.scanThrow()
     expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
       "▲ nexus:tsconfig We could not find a \\"tsconfig.json\\" file
       ▲ nexus:tsconfig We scaffolded one for you at __DYNAMIC__/tsconfig.json
@@ -142,7 +155,7 @@ describe('tsconfig', () => {
         include: ['.'],
       }),
     })
-    await ctx.scan()
+    await ctx.scanThrow()
     expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
       "▲ nexus:tsconfig You have set compilerOptions.tsBuildInfoFile in your tsconfig.json but it will be ignored by Nexus. Nexus manages this value internally.
       ▲ nexus:tsconfig You have set compilerOptions.incremental in your tsconfig.json but it will be ignored by Nexus. Nexus manages this value internally.
@@ -154,7 +167,7 @@ describe('tsconfig', () => {
     ctx.setup({
       'tsconfig.json': '',
     })
-    const layout = await ctx.scan()
+    const layout = await ctx.scanThrow()
     expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
       "▲ nexus:tsconfig You have not setup the Nexus TypeScript Language Service Plugin. Add this to your tsconfig compiler options:
 
@@ -176,7 +189,7 @@ describe('tsconfig', () => {
       }),
     })
 
-    await ctx.scan()
+    await ctx.scanThrow()
     expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
       "▲ nexus:tsconfig You have not added the Nexus TypeScript Language Service Plugin to your configured TypeScript plugins. Add this to your tsconfig compiler options:
 
@@ -190,7 +203,7 @@ describe('tsconfig', () => {
     ctx.setup({
       'tsconfig.json': 'bad json',
     })
-    await ctx.scan()
+    await ctx.scanThrow()
     expect(stripAnsi(mockedStdoutBuffer)).toMatchInlineSnapshot(`
       "✕ nexus:tsconfig Unable to read your tsconifg.json
 
@@ -217,7 +230,7 @@ describe('tsconfig', () => {
     ctx.setup({
       'tsconfig.json': '{ "exclude": "bad" }',
     })
-    await ctx.scan()
+    await ctx.scanThrow()
     expect(stripAnsi(mockedStdoutBuffer)).toMatchInlineSnapshot(`
       "▲ nexus:tsconfig You have not setup the Nexus TypeScript Language Service Plugin. Add this to your tsconfig compiler options:
 
@@ -247,7 +260,7 @@ it('fails if no entrypoint and no graphql modules', async () => {
     },
   })
 
-  await ctx.scan()
+  await ctx.scanThrow()
 
   expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
     "■ nexus:layout We could not find any modules that imports 'nexus' or app.ts entrypoint
@@ -284,7 +297,7 @@ it('finds nested nexus modules', async () => {
     },
   })
 
-  const result = await ctx.scan()
+  const result = await ctx.scanThrow()
 
   expect(result.nexusModules).toMatchInlineSnapshot(`
     Array [
@@ -300,13 +313,13 @@ it('finds nested nexus modules', async () => {
 
 it('detects yarn as package manager', async () => {
   ctx.setup({ ...fsTsConfig, 'app.ts': '', 'yarn.lock': '' })
-  const result = await ctx.scan()
+  const result = await ctx.scanThrow()
   expect(result.packageManagerType).toMatchInlineSnapshot(`"yarn"`)
 })
 
 it('finds app.ts entrypoint', async () => {
   ctx.setup({ ...fsTsConfig, 'app.ts': '' })
-  const result = await ctx.scan()
+  const result = await ctx.scanThrow()
   expect(result.app).toMatchInlineSnapshot(`
     Object {
       "exists": true,
@@ -317,7 +330,7 @@ it('finds app.ts entrypoint', async () => {
 
 it('set app.exists = false if no entrypoint', async () => {
   await ctx.setup({ ...fsTsConfig, 'graphql.ts': '' })
-  const result = await ctx.scan()
+  const result = await ctx.scanThrow()
   expect(result.app).toMatchInlineSnapshot(`
     Object {
       "exists": false,
@@ -328,7 +341,7 @@ it('set app.exists = false if no entrypoint', async () => {
 
 it('uses custom relative entrypoint when defined', async () => {
   await ctx.setup({ ...fsTsConfig, 'index.ts': `console.log('entrypoint')` })
-  const result = await ctx.scan({ entrypointPath: './index.ts' })
+  const result = await ctx.scanThrow({ entrypointPath: './index.ts' })
   expect(result.app).toMatchInlineSnapshot(`
     Object {
       "exists": true,
@@ -339,7 +352,7 @@ it('uses custom relative entrypoint when defined', async () => {
 
 it('uses custom absolute entrypoint when defined', async () => {
   await ctx.setup({ ...fsTsConfig, 'index.ts': `console.log('entrypoint')` })
-  const result = await ctx.scan({ entrypointPath: ctx.fs.path('index.ts') })
+  const result = await ctx.scanThrow({ entrypointPath: ctx.fs.path('index.ts') })
   expect(result.app).toMatchInlineSnapshot(`
     Object {
       "exists": true,
@@ -350,28 +363,18 @@ it('uses custom absolute entrypoint when defined', async () => {
 
 it('fails if custom entrypoint does not exist', async () => {
   await ctx.setup({ ...fsTsConfig, 'index.ts': `console.log('entrypoint')` })
-  await ctx.scan({ entrypointPath: './wrong-path.ts' })
-  expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
-    "✕ nexus Entrypoint does not exist  --  path: '__DYNAMIC__/wrong-path.ts'
-
-
-    --- process.exit(1) ---
-
-    "
-  `)
+  const result = await ctx.scan({ entrypointPath: './wrong-path.ts' })
+  expect(JSON.stringify(result)).toMatchInlineSnapshot(
+    `"{\\"_tag\\":\\"Left\\",\\"left\\":{\\"message\\":\\"Entrypoint does not exist\\",\\"context\\":{\\"path\\":\\"__DYNAMIC__/wrong-path.ts\\"}}}"`
+  )
 })
 
 it('fails if custom entrypoint is not a .ts file', async () => {
   await ctx.setup({ ...fsTsConfig, 'index.ts': ``, 'index.js': `console.log('entrypoint')` })
-  await ctx.scan({ entrypointPath: './index.js' })
-  expect(mockedStdoutBuffer).toMatchInlineSnapshot(`
-    "✕ nexus Entrypoint must be a .ts file  --  path: '__DYNAMIC__/index.js'
-
-
-    --- process.exit(1) ---
-
-    "
-  `)
+  const result = await ctx.scan({ entrypointPath: './index.js' })
+  expect(JSON.stringify(result)).toMatchInlineSnapshot(
+    `"{\\"_tag\\":\\"Left\\",\\"left\\":{\\"message\\":\\"Entrypoint must be a .ts file\\",\\"context\\":{\\"path\\":\\"__DYNAMIC__/index.js\\"}}}"`
+  )
 })
 
 it('does not take custom entrypoint as nexus module if contains a nexus import', async () => {
@@ -380,7 +383,7 @@ it('does not take custom entrypoint as nexus module if contains a nexus import',
     'app.ts': `import { schema } from 'nexus'`,
     'graphql.ts': `import { schema } from 'nexus'`,
   })
-  const result = await ctx.scan({ entrypointPath: './app.ts' })
+  const result = await ctx.scanThrow({ entrypointPath: './app.ts' })
   expect({
     app: result.app,
     nexusModules: result.nexusModules,
@@ -400,7 +403,7 @@ it('does not take custom entrypoint as nexus module if contains a nexus import',
 describe('build output', () => {
   it(`defaults to .nexus/build`, async () => {
     await ctx.setup({ ...fsTsConfig, 'graphql.ts': '' })
-    const result = await ctx.scan()
+    const result = await ctx.scanThrow()
 
     expect({
       tsOutputDir: result.build.tsOutputDir,
@@ -426,7 +429,7 @@ describe('build output', () => {
       }),
       'graphql.ts': '',
     })
-    const result = await ctx.scan()
+    const result = await ctx.scanThrow()
 
     expect({
       tsOutputDir: result.build.tsOutputDir,
@@ -451,7 +454,7 @@ describe('build output', () => {
       }),
       'graphql.ts': '',
     })
-    const result = await ctx.scan({ buildOutput: 'custom-output' })
+    const result = await ctx.scanThrow({ buildOutput: 'custom-output' })
 
     expect({
       tsOutputDir: result.build.tsOutputDir,
@@ -470,13 +473,13 @@ describe('build output', () => {
 describe('source root', () => {
   it('defaults to project dir', async () => {
     ctx.setup({ 'tsconfig.json': '' })
-    const result = await ctx.scan()
+    const result = await ctx.scanThrow()
     expect(result.sourceRoot).toEqual('__DYNAMIC__')
     expect(result.projectRoot).toEqual('__DYNAMIC__')
   })
   it('honours the value in tsconfig rootDir', async () => {
     ctx.setup({ 'tsconfig.json': tsconfigContent({ compilerOptions: { rootDir: 'api' } }) })
-    const result = await ctx.scan()
+    const result = await ctx.scanThrow()
     expect(result.sourceRoot).toMatchInlineSnapshot(`"__DYNAMIC__/api"`)
   })
 })
