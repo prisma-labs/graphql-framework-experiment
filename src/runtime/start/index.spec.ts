97561c96445444c76dae391bc4f6a239c5c5854d
Jason Kuhrt <jasonkuhrt@me.com>
2020-06-30 23:26:51 -0400

    add plugin case

--- src/runtime/start/index.spec.ts
+++ src/runtime/start/index.spec.ts
@@ -1,6 +1,7 @@
 import { rightOrThrow } from '@nexus/logger/dist/utils'
 import * as Layout from '../../lib/layout/layout'
 import { tsconfigTemplate } from '../../lib/layout/tsconfig'
+import { Manifest } from '../../lib/plugin'
 import * as TC from '../../lib/test-context'
 import { createStartModuleContent } from './'
 
@@ -36,6 +37,56 @@ describe('createStartModuleContent', () => {
 
       // Import the user's app module
       require(\\"./app\\")
+
+
+      app.assemble()
+      app.start()"
+    `)
+  })
+
+  it('with plugins', async () => {
+    ctx.fs.write('packge.json', { name: 'app', version: '0.0.0' })
+    ctx.fs.write('app.ts', '')
+    ctx.fs.write('tsconfig.json', tsconfigTemplate({ outRootRelative: 'build', sourceRootRelative: '.' }))
+    const layout = await Layout.create({ cwd: ctx.fs.cwd() }).then(rightOrThrow)
+    const pluginManifest = {
+      name: 'nexus-plugin-a',
+      runtime: {
+        export: 'a',
+        module: ctx.fs.path('node_modules/nexus-plugin-a/dist/runtime.js'),
+      },
+    } as Manifest
+    const sm = createStartModuleContent({
+      internalStage: 'build',
+      runtimePluginManifests: [pluginManifest],
+      layout,
+    })
+    expect(sm).toMatchInlineSnapshot(`
+      "// GENERATED NEXUS START MODULE
+
+
+      // Run framework initialization side-effects
+      // Also, import the app for later use
+      import app from \\"nexus\\")
+
+
+      // Last resort error handling
+      process.once('uncaughtException', error => {
+        app.log.fatal('uncaughtException', { error: error })
+        process.exit(1)
+      })
+
+      process.once('unhandledRejection', error => {
+        app.log.fatal('unhandledRejection', { error: error })
+        process.exit(1)
+      })
+
+
+      // Import the user's app module
+      require(\\"./app\\")
+
+
+      import { a as plugin_0 } from 'nexus-plugin-a/dist/runtime'
 
 
       app.assemble()
