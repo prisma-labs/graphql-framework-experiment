7efef8d23c75ae87fcdea7ce9eb6f1dbdb8a1328
Flavian Desverne <flavian.desverne@epitech.eu>
2020-07-06 18:33:29 +0200

    readdir c/users

--- path.js
+++ path.js
@@ -4,5 +4,6 @@ const fs = require('fs')
 console.log({
     tmp: os.tmpdir(),
     runner1: fs.statSync('C:\\Users\\RUNNER~1'),
-    realPath: fs.realpathSync('C:\\Users\\RUNNER~1')
+    realPath: fs.realpathSync('C:\\Users\\RUNNER~1'),
+    dir: fs.readdirSync('C:\\Users')
 })
