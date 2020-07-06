cfce4e7f9b3b335940c60cbe73a6b90ec480b27d
Flavian Desverne <flavian.desverne@epitech.eu>
2020-07-06 19:31:29 +0200

    3rd attempt...

--- src/lib/test-context/tmp-dir.ts
+++ src/lib/test-context/tmp-dir.ts
@@ -13,23 +13,10 @@ export const tmpDir = (opts?: { prefix: string }) =>
   createContributor<TmpDirDeps, TmpDirContribution>(() => {
     // Huge hack to force the tmpdir to be in its "long" form in the GH CI for windows
     if (process.env.CI !== undefined && os.platform() === 'win32') {
-      process.env.TMP = 'C:\\Users\\runneradmin\\AppData\\Local\\Temp'
-      process.env.TEMP = 'C:\\Users\\runneradmin\\AppData\\Local\\Temp'
+      return { tmpDir: 'C:\\Users\\runneradmin\\AppData\\Local\\Temp' }
     }
-
-    console.log({
-      ci: process.env.CI,
-      os: os.platform(),
-      TMP: process.env.TMP,
-      TEMP: process.env.TEMP,
-      tmpdir: os.tmpdir()
-    })
   
     const tmpDir = getTmpDir(opts?.prefix)
-
-    console.log({
-      tmpDir
-    })
 
     fs.dir(tmpDir)
 
