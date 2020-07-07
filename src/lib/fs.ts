f5aae2849e70287a042b8d84153bbd68e8056ea9
Jason Kuhrt <jasonkuhrt@me.com>
2020-07-07 12:23:15 -0400

    perf: skip node_modules when finding app module

--- src/lib/fs.ts
+++ src/lib/fs.ts
@@ -1,3 +1,4 @@
+import * as Glob from 'fast-glob'
 import * as NodeFS from 'fs'
 import * as FS from 'fs-jetpack'
 import * as OS from 'os'
@@ -168,17 +169,16 @@ export function sourceFilePathFromTranspiledPath({
   return Path.join(rootDir, maybeAppFolders, tsFileName)
 }
 
-export function findFile(
-  fileNames: string | string[],
-  config: { ignore?: string[]; projectRoot: string }
-): null | string {
-  const paths = Array.isArray(fileNames) ? fileNames : [fileNames]
-  const projectRoot = config.projectRoot
-  const localFs = FS.cwd(projectRoot)
-
-  const foundFiles = localFs.find({
-    matching: [...paths, '!node_modules/**/*', '!.yalc/**/*', ...(config?.ignore?.map((i) => `!${i}`) ?? [])],
-  })
+/**
+ * Find the given file within the directory tree under the given root path (cwd).
+ *
+ * Dot-folders, dot-files, node_modules are all always ignored
+ */
+export function findFile(pattern: string, config: { ignore?: string[]; cwd: string }): null | string {
+  const projectRoot = config.cwd
+  const cwd = config.cwd
+  const ignore = ['node_modules/**', ...(config.ignore ?? [])]
+  const foundFiles = Glob.sync(pattern, { cwd, ignore, absolute: true, dot: false })
 
   // TODO: What if several files were found?
   if (foundFiles.length > 0) {
