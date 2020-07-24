053c41c4577e001420a2807589aafd88f41b7c2b
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-07-24 11:26:45 +0200

    fix review

--- src/runtime/server/settings.ts
+++ src/runtime/server/settings.ts
@@ -3,6 +3,7 @@ import { CorsOptions as OriginalCorsOption } from 'cors'
 import * as Process from '../../lib/process'
 import * as Utils from '../../lib/utils'
 import { log as serverLogger } from './logger'
+import { LiteralUnion } from 'type-fest'
 
 const log = serverLogger.child('settings')
 
@@ -10,36 +11,50 @@ export type PlaygroundSettings = {
   settings?: Omit<Partial<Exclude<PlaygroundRenderPageOptions['settings'], undefined>>, 'general.betaUpdates'>
 }
 
+export type HTTPMethods = LiteralUnion<'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'HEAD', string>
+
 export type CorsSettings = {
+  /**
+   * Enable or disable CORS.
+   * 
+   * @default true
+   */
+  enabled?: boolean
   /**
    * Configures the Access-Control-Allow-Origin CORS header. Possible values:
    *
    * Boolean - set origin to true to reflect the request origin, as defined by req.header('Origin'), or set it to false to disable CORS.
+   * 
    * String - set origin to a specific origin. For example if you set it to "http://example.com" only requests from "http://example.com" will be allowed.
+   * 
    * RegExp - set origin to a regular expression pattern which will be used to test the request origin. If it's a match, the request origin will be reflected. For example the pattern /example\.com$/ will reflect any request that is coming from an origin ending with "example.com".
+   * 
    * Array - set origin to an array of valid origins. Each origin can be a String or a RegExp. For example ["http://example1.com", /\.example2\.com$/] will accept any request from "http://example1.com" or from a subdomain of "example2.com".
+   * 
    * Function - set origin to a function implementing some custom logic. The function takes the request origin as the first parameter and a callback (called as callback(err, origin), where origin is a non-function value of the origin option) as the second.
    *
    */
-  origin?: OriginalCorsOption['origin']
+  origin?: OriginalCorsOption['origin'] // TODO: Improve function interface with promise-based callback
   /**
    * Configures the Access-Control-Allow-Methods CORS header.
-   *
-   * Expects a comma-delimited string (ex: 'GET,PUT,POST') or an array (ex: ['GET', 'PUT', 'POST']).
+   * 
+   * @example ['GET', 'PUT', 'POST']
    */
-  methods?: string | string[]
+  methods?: string | HTTPMethods[]
   /**
    * Configures the Access-Control-Allow-Headers CORS header.
    *
-   * Expects a comma-delimited string (ex: 'Content-Type,Authorization') or an array (ex: ['Content-Type', 'Authorization']).
    * If not specified, defaults to reflecting the headers specified in the request's Access-Control-Request-Headers header.
+   * 
+   * @example ['Content-Type', 'Authorization']
    */
   allowedHeaders?: string | string[]
   /**
    * Configures the Access-Control-Expose-Headers CORS header.
    *
-   * Expects a comma-delimited string (ex: 'Content-Range,X-Content-Range') or an array (ex: ['Content-Range', 'X-Content-Range']).
    * If not specified, no custom headers are exposed.
+   * 
+   * @example ['Content-Range', 'X-Content-Range']
    */
   exposedHeaders?: string | string[]
   /**
@@ -224,6 +239,18 @@ function validateGraphQLPath(path: string): string {
   return outputPath
 }
 
+export function corsSettings(newSettings: SettingsInput['cors']): SettingsData['cors'] {
+  if (typeof newSettings === 'boolean') {
+    return newSettings
+  }
+
+  if (!newSettings || newSettings.enabled === false) {
+    return false
+  }
+
+  return newSettings
+}
+
 /**
  * Mutate the settings data
  */
@@ -235,7 +262,7 @@ export function changeSettings(state: SettingsData, newSettings: SettingsInput):
   state.port = updatedSettings.port
   state.startMessage = updatedSettings.startMessage
   state.graphql = graphqlSettings(updatedSettings.graphql)
-  state.cors = updatedSettings.cors
+  state.cors = corsSettings(updatedSettings.cors)
 }
 
 export function createServerSettingsManager() {
