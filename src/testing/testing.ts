f0129d286bca9b782979d2034114845d0f0f1a4d
Hays Clark <hays.clark@gmail.com>
2020-09-15 15:04:07 -0700

    refactor: extracting testing types into folder
    - updating test folder to match setup of internal ‘plugin’ library

--- src/testing/testing.ts
+++ src/testing/testing.ts
@@ -10,45 +10,9 @@ import { rightOrFatal } from '../lib/utils'
 import app from '../runtime'
 import { PrivateApp } from '../runtime/app'
 import { createDevAppRunner } from '../runtime/start'
+import { CreateTestContextOptions, TestContext, TestContextCore } from './types'
 
 const pluginLogger = rootLogger.child('plugin')
-
-export interface TestContextAppCore {
-  start(): Promise<void>
-  stop(): Promise<void>
-}
-
-export interface TestContextCore {
-  app: TestContextAppCore
-  client: GraphQLClient
-}
-
-declare global {
-  interface NexusTestContextApp extends TestContextAppCore { }
-
-  interface NexusTestContextRoot {
-    app: NexusTestContextApp
-    client: GraphQLClient
-  }
-}
-
-export type TestContext = NexusTestContextRoot
-
-export interface CreateTestContextOptions {
-  /**
-   * A path to the entrypoint of your app. Only necessary if the entrypoint falls outside of Nexus conventions.
-   * You should typically use this if you're using `nexus dev --entrypoint` or `nexus build --entrypoint`.
-   */
-  entrypointPath?: string
-  /**
-   * Nexus usually determines the project root by the first `package.json` found while traversing up the file system.
-   * In some cases, e.g. usage in a monorepo, this might not always be correct.
-   * For those cases, you can specify the `projectRoot` manually.
-   *
-   * Example: `await createTestContext({ projectRoot: path.join(__dirname, '../..') })`
-   */
-  projectRoot?: string
-}
 
 /**
  * Setup a test context providing utilities to query against your GraphQL API
@@ -76,7 +40,9 @@ export async function createTestContext(opts?: CreateTestContextOptions): Promis
   process.env.NEXUS_STAGE = 'dev'
 
   // todo figure out some caching system here, e.g. imagine jest --watch mode
-  const layout = rightOrFatal(await Layout.create({ entrypointPath: opts?.entrypointPath, projectRoot: opts?.projectRoot }))
+  const layout = rightOrFatal(
+    await Layout.create({ entrypointPath: opts?.entrypointPath, projectRoot: opts?.projectRoot })
+  )
   const pluginManifests = await PluginWorktime.getUsedPlugins(layout)
   const randomPort = await getPort({ port: getPort.makeRange(4000, 6000) })
   const privateApp = app as PrivateApp
@@ -84,7 +50,7 @@ export async function createTestContext(opts?: CreateTestContextOptions): Promis
   const forcedServerSettings = {
     port: randomPort,
     playground: false, // Disable playground during tests
-    startMessage() { }, // Make server silent
+    startMessage() {}, // Make server silent
   }
 
   // todo remove these settings hacks once we have https://github.com/graphql-nexus/nexus/issues/758
